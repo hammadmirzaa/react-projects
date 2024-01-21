88798fe15d58d6c079e7a409dfd099ec25751ed4
hammadmirzaa <hamadbaig25@gmail.com>
2024-01-22 01:46:14 +0500

    Reviews Final Is Completed, Impleted All the functionalities

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,9 +3,32 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.netlify.app/api/react-tours'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([])
+  const [loading, setLoading]= useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchTours();
+  },[]);
+  if(loading){
+    return <Loading/>
+  }
+  {
+  return <Tours/>
+  }
 }
 
 export default App
