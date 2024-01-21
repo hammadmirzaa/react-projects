58d0d1acbe93788282994a57da9aecb4acb63e8d
hammadmirzaa <hamadbaig25@gmail.com>
2024-01-21 13:40:25 +0500

    fixed clear all button

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <section className="container">
         <h3>{people.length} Birthdays Today</h3>
         <List people={people} />
-        <button onClick={() => setPeople([])}>Clear All</button>
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
